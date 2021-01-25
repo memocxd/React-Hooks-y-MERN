420c0ef07fe08ba344a6756cbdaec55a121e3a51
Kevin Zurita <memocxd@gmail.com>
2021-01-24 19:47:47 -0600

    Repaso de Async Await

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -10,10 +10,9 @@ body.style.cssText = 'background: #1d1d1d; color: #fff;';
 
 // FetchAPI
 
-const apiKey = 'UjjRUim31H8F0BRbp3ffHDR3TNFZW6u4';
+/* const apiKey = 'UjjRUim31H8F0BRbp3ffHDR3TNFZW6u4';
 const peticion = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
 
-console.log('Prueba');
 peticion
     .then( resp => resp.json() )
     .then( ({data}) => {
@@ -24,8 +23,29 @@ peticion
 
         document.body.append(img);
     })
-    .catch(console.warn);
+    .catch(console.warn); */
 
 
+
+// Async - Await
+
+const getImagen = async () => {
+    try {
+        const apiKey = 'UjjRUim31H8F0BRbp3ffHDR3TNFZW6u4';
+        const resp = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+        const {data} = await resp.json();
+        
+        const {url} = data.images.original;
+
+        const img = document.createElement('img');
+            img.src = url;
+
+            document.body.append(img);
+    } catch (error) {
+        console.warn('Ha ocurrido un error');
+    }
+}
+
+getImagen();
 
 
